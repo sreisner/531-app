a4677bcbf338489d20ee0be7abe27821d47364e7
sreisner <shawn.reisner@gmail.com>
2018-05-04 19:24:02 -0500

    Add basic navigation drawer

--- src/home/appBar531/appBar531.component.js
+++ src/home/appBar531/appBar531.component.js
@@ -4,9 +4,12 @@ import { withStyles } from 'material-ui/styles';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
-import Button from 'material-ui/Button';
 import IconButton from 'material-ui/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import AdjustIcon from '@material-ui/icons/Adjust';
+import { Drawer, List, ListItem, ListItemText, ListItemIcon } from 'material-ui';
+import Link from 'react-router-dom/Link';
 
 const styles = {
     root: {
@@ -21,22 +24,64 @@ const styles = {
     },
 };
 
-function AppBar531(props) {
-    const { classes } = props;
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    {<IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-                        <MenuIcon />
-                    </IconButton>}
-                    <Typography variant="title" color="inherit" className={classes.flex}>
-                        5/3/1
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+class AppBar531 extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            drawerIsOpen: false
+        };
+
+        this.toggleDrawer = this.toggleDrawer.bind(this);
+    }
+
+    toggleDrawer() {
+        this.setState(prevState => ({drawerIsOpen: !prevState.drawerIsOpen}));
+    }
+
+    render() {
+        const { classes } = this.props;
+        const { drawerIsOpen } = this.state;
+        
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" onClick={this.toggleDrawer}>
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="title" color="inherit" className={classes.flex}>
+                            5/3/1
+                        </Typography>
+                        <Drawer
+                            open={drawerIsOpen}
+                            onClose={this.toggleDrawer}
+                            anchor="left"
+                            >
+                            <List>
+                                <Link to="/">
+                                    <ListItem button>
+                                        <ListItemIcon>
+                                            <HomeIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary="Home" />
+                                    </ListItem>
+                                </Link>
+                                <Link to="/calculate-lifts">
+                                    <ListItem button>
+                                        <ListItemIcon>
+                                            <AdjustIcon />
+                                        </ListItemIcon>
+                                        <ListItemText primary="Calculate Lifts" />
+                                    </ListItem>
+                                </Link>
+                            </List>
+                        </Drawer>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
 }
 
 AppBar531.propTypes = {
--- src/home/home.component.js
+++ src/home/home.component.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import AppBar from './appBar531/appBar531.component';
+import AppBar531 from './appBar531/appBar531.component';
 
 class Home extends React.Component {
     render() {
         return (
-            <AppBar />
+            <div>
+                <AppBar531 />
+            </div>
         );
     }
 }

0be9655cc38996daca0aae99b60fb646ca5289d7
Shawn Reisner <shawn.reisner@gmail.com>
2018-05-09 08:04:39 -0500

    Add responsive Generate button

--- src/pages/beginCycle/beginCycle.component.js
+++ src/pages/beginCycle/beginCycle.component.js
@@ -6,10 +6,21 @@ import { TextField, Select, MenuItem, Typography, FormControl, InputLabel } from
 import { Grid } from 'material-ui';
 import { UsersService } from '../../services/api/users/users.service';
 import { TitleCard } from './components/titleCard.component';
+import { Button } from 'material-ui';
 
 const styles = theme => ({
-    grid: {
-        padding: theme.spacing.unit * 3
+    form: theme.mixins.gutters({
+        paddingTop: theme.spacing.unit * 3
+    }),
+    submitContainer: {
+        paddingTop: theme.spacing.unit * 2,
+        paddingBottom: theme.spacing.unit * 2
+    },
+    submitButton: {
+        [theme.breakpoints.only('xs')]: {
+            width: '100%'
+        },
+        float: 'right'
     }
 });
 
@@ -31,13 +42,21 @@ class BeginCycle extends React.Component {
             selectedTemplateId: 0
         };
 
+        this.loadTrainingMaxes = this.loadTrainingMaxes.bind(this);
+        this.loadTemplates = this.loadTemplates.bind(this);
         this.handleTrainingMaxChange = this.handleTrainingMaxChange.bind(this);
         this.handleTemplateChange = this.handleTemplateChange.bind(this);
         this.handleOptionChange = this.handleOptionChange.bind(this);
+        this.handleFormSubmission = this.handleFormSubmission.bind(this);
         this.getSelectedTemplate = this.getSelectedTemplate.bind(this);
     }
 
     componentDidMount() {
+        this.loadTrainingMaxes();
+        this.loadTemplates();
+    }
+
+    loadTrainingMaxes() {
         UsersService
             .getTrainingMaxes('current')
             .then(trainingMaxes => this.setState(prevState => ({ 
@@ -47,7 +66,9 @@ class BeginCycle extends React.Component {
                     ...trainingMaxes
                 }
             })));
+    }
 
+    loadTemplates() {
         setTimeout(() => this.setState({
             loadingTemplates: false,
             templates: [{
@@ -125,124 +146,151 @@ class BeginCycle extends React.Component {
         console.log(this.state.templates);
     }
 
+    handleFormSubmission(event) {
+        event.preventDefault();
+        console.log(event);
+    }
+
     getSelectedTemplate() {
         const { templates, selectedTemplateId } = this.state;
         return templates.find(t => t.id === selectedTemplateId);
     }
 
+    formIsValid() {
+        const { selectedTemplateId } = this.state;
+        const { squat, deadlift, bench, press } = this.state.trainingMaxes;
+        return squat && deadlift && bench && press && selectedTemplateId;
+    }
+
     render() {
         const { classes } = this.props;
-        const {
-            trainingMaxes,
-            selectedTemplateId,
-            loadingTemplates,
-            templates
-        } = this.state;
-
-        const {
-            squat,
-            deadlift,
-            bench,
-            press
-        } = trainingMaxes;
+        const { selectedTemplateId, loadingTemplates, templates } = this.state;
+        const { squat, deadlift, bench, press } = this.state.trainingMaxes;
 
         const selectedTemplate = this.getSelectedTemplate();
 
         return (
             <div>
                 <AppBar531 title="Begin Cycle" />
-                <Grid container className={classes.grid}>
-                    <Grid item xs={12} md={4}>
-                        <TitleCard title="Training Maxes">
-                            <TextField
-                                label="Squat TM"
-                                value={squat}
-                                name="squat"
-                                type="number"
-                                onChange={this.handleTrainingMaxChange} />
-                            <TextField
-                                label="Deadlift TM"
-                                value={deadlift}
-                                name="deadlift"
-                                type="number"
-                                onChange={this.handleTrainingMaxChange} />
-                            <TextField
-                                label="Bench Press TM"
-                                value={bench}
-                                name="bench"
-                                type="number"
-                                onChange={this.handleTrainingMaxChange} />
-                            <TextField
-                                label="Overhead Press TM"
-                                value={press}
-                                name="press"
-                                type="number"
-                                onChange={this.handleTrainingMaxChange} />
-                        </TitleCard>
-                    </Grid>
+                <form
+                    className={classes.form}
+                    noValidate
+                    onSubmit={this.handleFormSubmission}
+                >
+                    <Grid container>
+                        <Grid item xs={12} md={4}>
+                            <TitleCard title="Training Maxes">
+                                <TextField
+                                    label="Squat TM"
+                                    value={squat}
+                                    name="squat"
+                                    type="number"
+                                    onChange={this.handleTrainingMaxChange}
+                                    required
+                                    error={!squat} />
+                                <TextField
+                                    label="Deadlift TM"
+                                    value={deadlift}
+                                    name="deadlift"
+                                    type="number"
+                                    onChange={this.handleTrainingMaxChange}
+                                    required
+                                    error={!deadlift} />
+                                <TextField
+                                    label="Bench Press TM"
+                                    value={bench}
+                                    name="bench"
+                                    type="number"
+                                    onChange={this.handleTrainingMaxChange}
+                                    required
+                                    error={!bench} />
+                                <TextField
+                                    label="Overhead Press TM"
+                                    value={press}
+                                    name="press"
+                                    type="number"
+                                    onChange={this.handleTrainingMaxChange}
+                                    required
+                                    error={!press} />
+                            </TitleCard>
+                        </Grid>
 
-                    <Grid item xs={12} md={4}>
-                        <TitleCard title="Template">
-                            <FormControl>
-                            <InputLabel></InputLabel>
-                                <Select
-                                    value={selectedTemplateId}
-                                    onChange={this.handleTemplateChange}
-                                    disabled={loadingTemplates}
-                                >
-                                    {templates.map(t =>
-                                        <MenuItem
-                                            key={t.id}
-                                            value={t.id}
-                                        >
-                                            {t.name}
-                                        </MenuItem>
-                                    )}
-                                </Select>
-                            </FormControl>
-                        </TitleCard>
-                    </Grid>
+                        <Grid item xs={12} md={4}>
+                            <TitleCard title="Template">
+                                <FormControl>
+                                <InputLabel></InputLabel>
+                                    <Select
+                                        value={selectedTemplateId}
+                                        onChange={this.handleTemplateChange}
+                                        disabled={loadingTemplates}
+                                    >
+                                        {templates.map(t =>
+                                            <MenuItem
+                                                key={t.id}
+                                                value={t.id}
+                                            >
+                                                {t.name}
+                                            </MenuItem>
+                                        )}
+                                    </Select>
+                                </FormControl>
+                            </TitleCard>
+                        </Grid>
 
-                    <Grid item xs={12} md={4}>
-                        <TitleCard title="Options">
-                            {!selectedTemplate ? (
-                                <Typography variant="caption">
-                                    You must select a template to customize options.
-                                </Typography>
-                            ) : selectedTemplate.options.length === 0 ? (
-                                <Typography variant="caption">
-                                    There are no options available for this template.
-                                </Typography>
-                            ) : (
-                                selectedTemplate.options.map(o => {
-                                    if (o.type === 'select') {
-                                        return (
-                                            <FormControl key={o.name}>
-                                                <InputLabel htmlFor={o.name}>{o.name}</InputLabel>
-                                                <Select
-                                                    value={o.value}
-                                                    onChange={this.handleOptionChange}
-                                                    inputProps={{name: o.name}}
-                                                >
-                                                    {o.values.map(v => (
-                                                        <MenuItem
-                                                            key={v.value}
-                                                            value={v.value}
-                                                        >
-                                                            {v.display}
-                                                        </MenuItem>
-                                                    ))}
-                                                </Select>
-                                            </FormControl>
-                                        );
-                                    } else {
-                                        return null;
-                                    }
-                                })
-                            )}
-                        </TitleCard>
+                        <Grid item xs={12} md={4}>
+                            <TitleCard title="Options">
+                                {!selectedTemplate ? (
+                                    <Typography variant="caption">
+                                        You must select a template to customize options.
+                                    </Typography>
+                                ) : selectedTemplate.options.length === 0 ? (
+                                    <Typography variant="caption">
+                                        There are no options available for this template.
+                                    </Typography>
+                                ) : (
+                                    selectedTemplate.options.map(o => {
+                                        if (o.type === 'select') {
+                                            return (
+                                                <FormControl key={o.name}>
+                                                    <InputLabel htmlFor={o.name}>{o.name}</InputLabel>
+                                                    <Select
+                                                        value={o.value}
+                                                        onChange={this.handleOptionChange}
+                                                        inputProps={{name: o.name}}
+                                                    >
+                                                        {o.values.map(v => (
+                                                            <MenuItem
+                                                                key={v.value}
+                                                                value={v.value}
+                                                            >
+                                                                {v.display}
+                                                            </MenuItem>
+                                                        ))}
+                                                    </Select>
+                                                </FormControl>
+                                            );
+                                        } else {
+                                            return null;
+                                        }
+                                    })
+                                )}
+                            </TitleCard>
+                        </Grid>
                     </Grid>
-                </Grid>
+                    <Grid container className={classes.submitContainer} justify="flex-end">
+                        <Grid item xs={12}>
+                            <Button
+                                className={classes.submitButton}
+                                variant="raised"
+                                color="primary"
+                                type="submit"
+                                disabled={!this.formIsValid()}
+                            >
+                                Generate
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </form>
             </div>
         );
     }

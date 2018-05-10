ed215058ee4cd890ece19a33f8f2d9418f34589d
Shawn Reisner <shawn.reisner@gmail.com>
2018-05-10 11:14:18 -0500

    Allow cycles to be generated with more than one daily lift

--- src/pages/cycle/cycleGenerator.service.js
+++ src/pages/cycle/cycleGenerator.service.js
@@ -1,7 +1,7 @@
-function* nextLiftGenerator(liftOrder) {
+function* dailyLiftsGenerator(dailyLifts) {
   let index = 0;
   while (true) {
-    yield liftOrder[index++ % liftOrder.length];
+    yield dailyLifts[index++ % dailyLifts.length];
   }
 }
 
@@ -15,39 +15,53 @@ const generateWeeklySessions = (
   trainingMaxes,
   daysPerWeek
 ) => {
-  const liftGen = nextLiftGenerator(liftOrder);
+  const liftGen = dailyLiftsGenerator(liftOrder);
 
   return weeklyRepSchemes.map(week => ({
     sessions: [...Array(daysPerWeek)].map(_ => {
-      const lift = liftGen.next().value;
+      const dailyLifts = liftGen.next().value;
+
       return {
-        sets: week.map(day => ({
-          lift,
-          reps: day.reps,
-          sets: day.sets,
-          weight: calculateSetWeight(trainingMaxes[lift], day.percentage),
-        })),
+        sets: week.map(day => {
+          const lift = dailyLifts[day.liftIndex];
+
+          return {
+            lift,
+            reps: day.reps,
+            sets: day.sets,
+            weight: calculateSetWeight(trainingMaxes[lift], day.percentage),
+          };
+        }),
       };
     }),
   }));
 };
 
+/*
+The liftIndex is used for days programmed with different lifts.
+For example in Forever BBB, users have the option to do squats
+for the 5/3/1 sets (so that will end up being liftIndex 0) and
+deadlifts for the 5x10 sets (which will end up being liftIndex 1)
+*/
 const standard531WeeklyRepSchemes = [
   [
     {
       percentage: 0.65,
       reps: 5,
       sets: 1,
+      liftIndex: 0,
     },
     {
       percentage: 0.75,
       reps: 5,
       sets: 1,
+      liftIndex: 0,
     },
     {
       percentage: 0.85,
       reps: 5,
       sets: 1,
+      liftIndex: 0,
     },
   ],
   [
@@ -55,16 +69,19 @@ const standard531WeeklyRepSchemes = [
       percentage: 0.6,
       reps: 3,
       sets: 1,
+      liftIndex: 0,
     },
     {
       percentage: 0.7,
       reps: 3,
       sets: 1,
+      liftIndex: 0,
     },
     {
       percentage: 0.8,
       reps: 3,
       sets: 1,
+      liftIndex: 0,
     },
   ],
   [
@@ -72,22 +89,25 @@ const standard531WeeklyRepSchemes = [
       percentage: 0.75,
       reps: 5,
       sets: 1,
+      liftIndex: 0,
     },
     {
       percentage: 0.85,
       reps: 3,
       sets: 1,
+      liftIndex: 0,
     },
     {
       percentage: 0.95,
       reps: 1,
       sets: 1,
+      liftIndex: 0,
     },
   ],
 ];
 
 const foreverBBBConfig = options => ({
-  liftOrder: options.liftOrder,
+  dailyLifts: options.dailyLifts,
   daysPerWeek: options.daysPerWeek,
   jumpsThrows: 10,
   weeklyRepSchemes: standard531WeeklyRepSchemes.map(week => [
@@ -96,6 +116,7 @@ const foreverBBBConfig = options => ({
       percentage: options.supplementalTmPercentage,
       reps: 10,
       sets: 5,
+      liftIndex: options.slightlyLessBoring ? 1 : 0,
     },
   ]),
   assistance: {
@@ -124,7 +145,7 @@ const generateCycle = (templateId, trainingMaxes, options) => {
     jumpsThrows,
     weeklyRepSchemes,
     assistance,
-    liftOrder,
+    dailyLifts,
     daysPerWeek,
   } = config;
 
@@ -132,7 +153,7 @@ const generateCycle = (templateId, trainingMaxes, options) => {
     jumpsThrows,
     weeklySessions: generateWeeklySessions(
       weeklyRepSchemes,
-      liftOrder,
+      dailyLifts,
       trainingMaxes,
       daysPerWeek
     ),
@@ -143,3 +164,5 @@ const generateCycle = (templateId, trainingMaxes, options) => {
 export default {
   generateCycle,
 };
+
+// http://localhost:3000/cycle?squat=315&deadlift=405&bench=225&press=135&templateId=1&options={"daysPerWeek":3,"supplementalTmPercentage":0.4,"dailyLifts":[["squat","deadlift"],["bench","press"],["deadlift","squat"],["press","bench"]],"slightlyLessBoring":true}

969b96cf19beef0523ec4a3de78d2436d9c09dba
sreisner <shawn.reisner@gmail.com>
2018-05-12 13:44:45 -0500

    Move BrowserRouter to ReactDOM.render call at root of app

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import withRoot from './withRoot';
 import { withStyles } from 'material-ui/styles';
-import { BrowserRouter } from 'react-router-dom';
 import { Home } from './pages/home/home.component';
 import { Login } from './pages/login/login.component';
 import { PrivateRoute } from './core/privateRoute/privateRoute.component';
@@ -32,19 +31,17 @@ class App extends React.Component {
     const { classes } = this.props;
 
     return (
-      <BrowserRouter>
-        <div className={classes.root}>
-          <LoginRoute
-            exact
-            path="/login"
-            component={Login}
-            onSuccess={this.handleSuccessfulLogin}
-          />
-          <PrivateRoute exact path="/" component={Home} />
-          <PrivateRoute exact path="/begin-cycle" component={BeginCycle} />
-          <PrivateRoute exact path="/cycle" component={Cycle} />
-        </div>
-      </BrowserRouter>
+      <div className={classes.root}>
+        <LoginRoute
+          exact
+          path="/login"
+          component={Login}
+          onSuccess={this.handleSuccessfulLogin}
+        />
+        <PrivateRoute exact path="/" component={Home} />
+        <PrivateRoute exact path="/begin-cycle" component={BeginCycle} />
+        <PrivateRoute exact path="/cycle" component={Cycle} />
+      </div>
     );
   }
 }

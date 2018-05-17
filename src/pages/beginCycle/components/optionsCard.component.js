1b6348defb7679d75221aa7d504d903cc5f426ef
Shawn Reisner <shawn.reisner@gmail.com>
2018-05-17 09:21:35 -0500

    Convert OptionsCard into a stateless functional component

--- src/pages/beginCycle/components/optionsCard.component.js
+++ src/pages/beginCycle/components/optionsCard.component.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from 'material-ui/styles';
 import { TitleCard } from './titleCard.component';
 import { Typography, MenuItem } from 'material-ui';
 import { FormControl } from 'material-ui';
@@ -9,103 +7,64 @@ import { Select } from 'material-ui';
 import { FormControlLabel } from 'material-ui';
 import { Switch } from 'material-ui';
 
-const styles = theme => ({});
+let OptionsCard = props => {
+  const { selectedTemplate, selectedVariant, optionValues, onChange } = props;
 
-class OptionsCard extends React.Component {
-  constructor(props) {
-    super(props);
+  const templateOptions = selectedTemplate.options || [];
+  const variantOptions = selectedVariant.options || [];
+  const optionsMeta = [...templateOptions, ...variantOptions];
 
-    this.state = {};
-
-    this.getOptionsMetaDefaultValue = this.getOptionsMetaDefaultValue.bind(
-      this
-    );
-
-    this.buildOptionsMeta = this.buildOptionsMeta.bind(this);
-  }
-
-  getOptionsMetaDefaultValue(key) {
-    const optionsMeta = this.buildOptionsMeta();
-
-    return optionsMeta.find(m => m.key === key).defaultValue;
-  }
-
-  buildOptionsMeta() {
-    const { selectedTemplate, selectedVariant } = this.props;
-
-    const templateOptions = selectedTemplate.options || [];
-    const variantOptions = selectedVariant.options || [];
-
-    return [...templateOptions, ...variantOptions];
-  }
-
-  render() {
-    const {
-      selectedTemplate,
-      selectedVariant,
-      optionValues,
-      onChange,
-    } = this.props;
-
-    const optionsMeta = this.buildOptionsMeta();
-
-    return (
-      <TitleCard title="Options">
-        {!selectedTemplate._id || !selectedVariant.id ? (
-          <Typography variant="caption">
-            Select a template to customize options.
-          </Typography>
-        ) : optionsMeta.length === 0 ? (
-          <Typography variant="caption">
-            There are no options available for this template.
-          </Typography>
-        ) : (
-          optionsMeta.map(o => {
-            const value = optionValues[o.key];
-            if (o.type === 'select') {
-              return (
-                <FormControl key={o.key}>
-                  <InputLabel htmlFor={o.key}>{o.displayText}</InputLabel>
-                  <Select
-                    value={value}
-                    onChange={e => onChange(o.key, e.target.value)}
-                    inputProps={{ name: o.key }}
-                  >
-                    {o.values.map(v => (
-                      <MenuItem key={v.value} value={v.value}>
-                        {v.displayText}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                </FormControl>
-              );
-            } else if (o.type === 'boolean') {
-              return (
-                <FormControlLabel
-                  key={o.key}
-                  control={
-                    <Switch
-                      checked={value}
-                      onChange={e => onChange(o.key, e.target.checked)}
-                      value={value.toString()}
-                    />
-                  }
-                  label={o.displayText}
-                />
-              );
-            } else {
-              return null;
-            }
-          })
-        )}
-      </TitleCard>
-    );
-  }
-}
-
-OptionsCard.propTypes = {
-  classes: PropTypes.object.isRequired,
+  return (
+    <TitleCard title="Options">
+      {!selectedTemplate._id || !selectedVariant.id ? (
+        <Typography variant="caption">
+          Select a template to customize options.
+        </Typography>
+      ) : optionsMeta.length === 0 ? (
+        <Typography variant="caption">
+          There are no options available for this template.
+        </Typography>
+      ) : (
+        optionsMeta.map(o => {
+          const value = optionValues[o.key];
+          if (o.type === 'select') {
+            return (
+              <FormControl key={o.key}>
+                <InputLabel htmlFor={o.key}>{o.displayText}</InputLabel>
+                <Select
+                  value={value}
+                  onChange={e => onChange(o.key, e.target.value)}
+                  inputProps={{ name: o.key }}
+                >
+                  {o.values.map(v => (
+                    <MenuItem key={v.value} value={v.value}>
+                      {v.displayText}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            );
+          } else if (o.type === 'boolean') {
+            return (
+              <FormControlLabel
+                key={o.key}
+                control={
+                  <Switch
+                    checked={value}
+                    onChange={e => onChange(o.key, e.target.checked)}
+                    value={value.toString()}
+                  />
+                }
+                label={o.displayText}
+              />
+            );
+          } else {
+            return null;
+          }
+        })
+      )}
+    </TitleCard>
+  );
 };
 
-OptionsCard = withStyles(styles)(OptionsCard);
 export { OptionsCard };

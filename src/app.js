2a4a42f6bb7d52895ef7eceb9fc5767e5de279df
sreisner <shawn.reisner@gmail.com>
2018-05-22 19:35:16 -0500

    Update path names

--- src/app.js
+++ src/app.js
@@ -5,7 +5,6 @@ import { AppBar531 } from './core/appBar531/appBar531.component';
 import { Route, Switch } from 'react-router-dom';
 import { Cycle } from './views/cycle/cycle.component';
 import { CycleGenerator } from './views/cycleGenerator/cycleGenerator.component';
-import Dashboard from './views/dashboard/dashboard.component';
 import { Loading } from './core/loading/loading.component';
 import { withRouter } from 'react-router-dom';
 import { AuthConsumer } from './context/authContext.component.js';
@@ -32,8 +31,12 @@ class App extends React.Component {
             <div className={classes.root}>
               <AppBar531 title="5/3/1" />
               <Switch>
-                <Route exact path="/" component={Dashboard} />
-                <Route exact path="/cycle" component={Cycle} />
+                <Route exact path="/cycle/current" component={Cycle} />
+                <Route
+                  exact
+                  path="/cycle/generator"
+                  component={CycleGenerator}
+                />
                 <Route
                   exact
                   path="/cycle-generator"

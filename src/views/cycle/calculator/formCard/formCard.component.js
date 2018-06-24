d007b9fb26f606edc2d41b0083a1ce07913d7fb2
sreisner <shawn.reisner@gmail.com>
2018-06-24 17:43:45 -0500

    Add form card component

---
+++ src/views/cycle/calculator/formCard/formCard.component.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+const FormCard = () => (
+
+);
+
+export default FormCard;
+
+class FormCardContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  };
+  }
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default FormCard;
